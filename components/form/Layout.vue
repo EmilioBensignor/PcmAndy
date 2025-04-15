f583e8085fe3f3e6a16968fe3ec948ee53b668c6
Lio <liobensignor@gmail.com>
2025-04-15 18:23:57 -0300

    Fix prevent keydown enter

--- components/form/Layout.vue
+++ components/form/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="w-full flex flex-col items-center gap-4 lg:gap-6">
+    <form @keydown.enter.prevent class="w-full flex flex-col items-center gap-4 lg:gap-6">
         <slot />
     </form>
 </template>
